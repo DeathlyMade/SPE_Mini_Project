4c2d43d62f459a3677be4bb7893acf0c5e78519b
DeathlyMade <Divyam.Sareen@iiitb.ac.in>
2025-10-09 05:30:18 +0530

    trying something

--- frontend/science-calc/vite.config.ts
+++ frontend/science-calc/vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Proxy API calls to backend during dev (backend running on :8080)
+    // Proxy API calls to backend during dev (backend running on :8081)
     host:true,
     port: 5173,
     strictPort: false,
     proxy: {
       '/api': {
-        target: 'http://spe-backend-container:8081',
+        target: 'http://localhost:8081',
         changeOrigin: true,
         secure: false
       }
